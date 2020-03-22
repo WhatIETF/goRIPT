c0453ab9afad6fbe133627a5d5e895af4bbf75f6
snandaku <snandaku@cisco.com>
2020-03-21 23:26:46 -0700

    first stab at ad parse

--- api/advertisement.go
+++ api/advertisement.go
@@ -1,26 +1,75 @@
 package api
 
+import (
+	"errors"
+	"fmt"
+	"github.com/go-acme/lego/log"
+	"strconv"
+	"strings"
+)
 
-
-type AdvertismentInfo struct {
-
-}
+const (
+	DirectionIn = "in"
+	DirectionOut = "out"
+)
 
 // todo: support codec params
 type CodecInfo struct {
 	Codec string
 }
 
-type SourceCapability struct {
-	SourceId int
+type Capability struct {
+	Id int
+	Direction string
 	Codecs []CodecInfo
 }
-
-type SinkCapability struct {
-	SinkId int
-
+type AdvertisementInfo struct {
+	Caps []Capability
 }
 
-func (ad Advertisement) parse() {
+// Crude parse function. Needs to be revisited once ABNF is defined
+func (ad Advertisement) parse() (AdvertisementInfo, error) {
 
+	adLines := strings.Split(string(ad),"\n")
+	if len(adLines) == 0 {
+		return AdvertisementInfo{}, errors.New("error parsing advertisement")
+	}
+
+	log.Println("lines [%s]", adLines)
+	adInfo := AdvertisementInfo{}
+	for idx, line := range adLines {
+		var cap Capability
+		if len(line) == 0 {
+			continue
+		}
+		parts := strings.Split(line, " ")
+		log.Printf("\tparts [%s]", parts)
+
+		if len(parts) < 3 {
+			// need id, dir and atleast one codec
+			return adInfo, fmt.Errorf("line [%d] misses minimally required slots", idx)
+		}
+
+		id, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return adInfo, err
+		}
+		cap.Id = id
+		dir := strings.TrimRight(parts[1], ":")
+
+		if dir != DirectionIn && dir != DirectionOut {
+			return adInfo, fmt.Errorf("line [%d] malformed direction", idx)
+		}
+		cap.Direction = dir
+
+		// parse codes
+		var codecs []CodecInfo
+		for i := 2; i < len(parts); i++ {
+			codec := strings.TrimRight(parts[i], ";")
+			codecs = append(codecs, CodecInfo{codec})
+		}
+		cap.Codecs = codecs
+		adInfo.Caps = append(adInfo.Caps, cap)
+	}
+	return adInfo, nil
 }
